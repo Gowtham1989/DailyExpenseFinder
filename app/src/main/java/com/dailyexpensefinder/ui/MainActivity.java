b3ca940ddf6ddf27962999292003bc42add36328
Gowtham1989 <gowthammahadevaiah@gmail.com>
2019-05-18 20:27:51 +0530

    splash screen creation

---
+++ app/src/main/java/com/dailyexpensefinder/ui/MainActivity.java
@@ -0,0 +1,31 @@
+package com.dailyexpensefinder.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.dailyexpensefinder.R;
+import com.dailyexpensefinder.util.Constants;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    sleep(Constants.SPLASH_TIME *1000);
+                    Intent i=new Intent(MainActivity.this,StoreExpenses.class);
+                    startActivity(i);
+                } catch (Exception e) {
+
+                }
+            }
+        };
+        background.start();
+    }
+}
---
+++ app/src/main/java/com/dailyexpensefinder/ui/StoreExpenses.java
@@ -0,0 +1,18 @@
+package com.dailyexpensefinder.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+
+import com.dailyexpensefinder.R;
+
+public class StoreExpenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storeexpenses);
+    }
+
+}
